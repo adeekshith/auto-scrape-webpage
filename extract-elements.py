8d111098f8e6d1ba2766f92e8e882417b5846490
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-11-09 00:55:34 -0600

    Extracting the pod from which the given element and its siblings can be extracted.

--- extract-elements.py
+++ extract-elements.py
@@ -9,37 +9,60 @@ import urllib2
 import urllib
 
 
+def printSiblings(soupBowl):
+    numSiblings = 0
+    thisSiblings = soupBowl.next_siblings
+    for eachSibling in thisSiblings:
+        numSiblings += 1
+        print "\n\n-> "+str(eachSibling)
+        print "\t " + eachSibling.get_text()
+    return numSiblings
+
+
 def siblingCount(soupBowl):
     numSiblings = 0
     thisSiblings = soupBowl.next_siblings
     for eachSibling in thisSiblings:
-        print "\n\n-> "+str(eachSibling)
-        print "\t " + str(eachSibling.string)
         numSiblings += 1
     return numSiblings
 
 
+def checkGivenElementsFound(soupBowl, numSiblings):
+    while True:
+        if (siblingCount(soupBowl) + 1 == numSiblings):
+            return soupBowl
+        soupBowl = soupBowl.parent
+    return -1
+
+
 def answer(firstContent, numSiblings):
-    with open("test/test-file3.html", "r") as myfile:
+    with open("test/test-file2.html", "r") as myfile:
         html_doc = myfile.read().replace('\n', '')
     soup = BeautifulSoup(html_doc, "html.parser")
-    fadTitle = soup.find_all("a", text=firstContent)
-    while True:
-        if isinstance(fadTitle, list):
-            if len(fadTitle) < 1:
-                return -1
-            fadTitle = fadTitle[0]
-        if (siblingCount(fadTitle) + 1 == numSiblings):
-            return fadTitle
-        fadTitle = fadTitle.parent
-    return fadTitle
+    matchedElements = soup.find_all("a", text=firstContent)
+    if isinstance(matchedElements, list):
+        for eachMatchedEle in matchedElements:
+            thisResult = checkGivenElementsFound(eachMatchedEle, numSiblings)
+            if thisResult != -1:
+                return thisResult
+    else:
+        thisResult = checkGivenElementsFound(eachMatchedEle, numSiblings)
+        if thisResult != -1:
+            return thisResult
+    return -1
     # return fadTitle[0].get_text(', ').encode("utf-8")
 
 
 if __name__ == "__main__":
     print("Testing Element Extraction")
     print("________________________")
-    firstContent = "Privacy policy"
-    print(answer(firstContent, 6))
+    firstContent = "Software Engineer"
+    extractedMatchingElement = answer(firstContent, 6)
+    # Printing extracted matching element
+    print(extractedMatchingElement)
+    # Printing siblings
+    printSiblings(extractedMatchingElement)
+    print("\n\n-=-=-=-=-=- Extracted Matching Element -=-=-=-=-=-\n\n")
+    print(extractedMatchingElement.prettify())
     print("________________________")
     # print(amazonProductInfo('http://goo.gl/qOvGmp'))
